553310a6f707400c428524e5f3ff53e69a151af0
Ismael Coulibaly <60194532+Hy-5@users.noreply.github.com>
2023-10-31 09:46:26 -0500

    quiz selection page init

---
+++ src/pages/student-quiz-selection/index.tsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { SectionContainer } from '../../components';
+import Spinner from '../../components/common/spinner';
+import { RootState } from '../../store';
+import { setCredentials } from '../../store/features/auth/authSlice';
+import { useGetUserDetailsQuery } from '../../store/services/auth/atuhService';
+
+const StudQuizSelect = () => {
+  return (
+    <div>
+      Lorem Ipsum
+    </div>
+  );
+};
+
+export default StudQuizSelect;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,6 +7,8 @@ import { ProtectedRoute } from "../components";
 import { CourseQuizzes, DashBoard, Login } from "../pages";
 import CreateQuiz from "../pages/create-quiz";
 import StudentDashboard from "../pages/student-dashboard";
+import StudQuizSelect from "../pages/student-quiz-selection";
+
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -16,6 +18,7 @@ const router = createBrowserRouter(
         <Route path="/course/:id" element={<CourseQuizzes />} />
         <Route path="/course/:courseId/create-quiz" element={<CreateQuiz />} />
         <Route path="/student-dashboard" element={<StudentDashboard />} />
+        <Route path="/student-quiz-selection" element={<StudQuizSelect />} />
       </Route>
 
       <Route path="/login" element={<Login />} />
